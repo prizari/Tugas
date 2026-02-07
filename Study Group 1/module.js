6ab313187668d709850d95c6ba9220536a35e666
prizari <naufal.fadhil128@gmail.com>
2026-02-07 17:56:34 +0700

    Add files via upload

---
+++ Study Group 1/module.js
@@ -0,0 +1,19 @@
+// init project
+
+const umur = 22;
+const uang = 7000000;
+if (umur >= 21)
+{
+    console.log("boleh masuk");
+}
+else{
+    console.log("tidak boleh masuk");
+}
+
+if (uang > 5000000)
+{
+    console.log("uang cukup");
+}
+else{
+    console.log("uang tidak cukup");
+}
